627127ead3add26fa6332b74841e7058e6640476
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-05-18 12:22:47 +0300

    Fix answer spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Answer, type: :model do
-  let(:question) { create(:question, title: "title question", body: "body question") }
-  let(:answer) { create(:answer, body: "body answer", question: question) }
-
   it { should validate_presence_of :body }
 
-  it "validates answer belongs to question" do
-    expect(answer.question_id).to eq(question.id)
-  end
+  it { should belong_to(:question) }
 end
