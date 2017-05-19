73f04ed85168cfc8da905b7b023b36fc34321f6c
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-05-19 23:46:42 +0300

    Create controller answerss with specs

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,30 @@
+class AnswersController < ApplicationController
+  before_action :load_question
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.create(answer_params)
+    if @answer.save
+      redirect_to question_answers_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @answers = @question.answers
+  end
+
+  private
+
+    def load_question
+      @question = Question.find(params[:question_id])
+    end
+
+    def answer_params
+      params.require(:answer).permit(:body)
+    end
+end
---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe "GET #new" do
+    before { get :new, question_id: question }
+
+    it "assigns a new Answer to @answer" do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it "render new view" do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves new answers in database" do
+        expect { post :create,
+                 params: { question_id: question, answer: attributes_for(:answer).except(:question) } }.to change(Answer, :count).by(1)
+      end
+
+      it "redirect to index view" do
+        post :create, params: { question_id: question, answer: attributes_for(:answer).except(:question) }
+        expect(response).to redirect_to question_answers_path(assigns(:question))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't saves new answers in database" do
+        expect { post :create,
+                 params: { question_id: question, answer: { body: nil } }.not_to
+                 change(Answer, :count) }
+      end
+
+      it "re-renders new view" do
+        post :create, params: { question_id: question, answer: { body: nil } }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe "GET #index" do
+    let(:answers) { create_list(:answer, 3, question: question) }
+
+    before { get :index, params: { question_id: question } }
+
+    it "populates an array of all answers" do
+      expect(assigns(:answers)).to match_array(answers)
+    end
+
+    it "renders index view" do
+      expect(response).to render_template :index
+    end
+  end
+end
