e574f21398d82e10387a9049f10f2198725f59a7
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-05-24 15:13:57 +0300

    User can sign out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "User sign out", '
+  In order to end of use site
+  As an authenticated User
+  I want to be able to sign out
+' do
+  given(:user) { create(:user) }
+
+  scenario "Authenticated user sign out" do
+    sign_in(user)
+
+    click_on "Log out"
+
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).not_to have_content("Log out")
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -13,6 +14,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
+  config.include AcceptanceMacros, type: :feature
 end
 
 require "shoulda/matchers"
