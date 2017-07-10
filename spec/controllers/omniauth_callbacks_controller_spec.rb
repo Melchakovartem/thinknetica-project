4a1a44fab70619c0bd267999e1687c40c1d01b73
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-07-11 01:38:14 +0300

    Fix omniauth callbacks controller spec

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -3,76 +3,91 @@ require 'rails_helper'
 RSpec.describe OmniauthCallbacksController, type: :controller do
   let(:user) { create(:user) }
 
-  describe 'facebook auth' do
+  describe "facebook auth" do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       request.env["omniauth.auth"] = mock_auth_hash(:facebook)
     end
 
-    context 'if new user' do
+    context "if new user" do
       before do
         auth = mock_auth_hash(:facebook)
-        get :facebook
       end
 
-      it 'redeirects to root path' do
+      it "redeirects to root path" do
+        get :facebook
         expect(response).to redirect_to(root_path)
       end
 
-      it 'doesnt signin user' do
-        expect(controller.current_user).to eq User.first
+      it "creates new user" do
+        expect do
+          get :facebook
+        end.to change(User, :count).by(1)
       end
     end
 
-    context 'with existing facebook user' do
+    context "if facebook user is exist" do
       before do
         auth = mock_auth_hash(:facebook)
         authorization = create(:authorization, user: user, provider: auth.provider, uid: auth.uid)
-        get :facebook
       end
 
-      it 'redeirects to rootpath' do
+      it "redeirects to root path" do
+        get :facebook
         expect(response).to redirect_to(root_path)
       end
 
-      it 'signin user' do
-        expect(controller.current_user).to eq user
+      it "doesn't creates new user" do
+        expect do
+          get :facebook
+        end.to_not change(User, :count)
       end
     end
   end
 
-  describe 'vkontakte auth' do
+  describe "vkontakte auth" do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       request.env["omniauth.auth"] = mock_auth_hash(:vkontakte)
     end
 
-    context 'with a new VK user' do
-      before do
+    context "if new user" do
+      it "redeirects to enter email path" do
         get :vk
+        expect(response).to render_template "application/enter_email"
       end
 
-      it 'redeirects to new_user_path' do
-        expect(response).to render_template 'application/enter_email'
+      it "doesn't creates new user" do
+        expect do
+          get :vk
+        end.to_not change(User, :count)
       end
 
-      it 'doesnt signin user' do
+      it "doesn't sign in user" do
+        get :vk
         expect(controller.current_user).to eq nil
       end
     end
 
-    context 'with existing VK user' do
+    context "if vkontakte user is exist" do
       before do
         auth = mock_auth_hash(:vkontakte)
         authorization = create(:authorization, user: user, provider: auth.provider, uid: auth.uid)
-        get :vk
       end
 
-      it 'redeirects to rootpath' do
+      it "redeirects to root path" do
+        get :vk
         expect(response).to redirect_to(root_path)
       end
 
-      it 'signin user' do
+      it "doesn't create new user" do
+        expect do
+          get :vk
+        end.to_not change(User, :count)
+      end
+
+      it "sign's in user" do
+        get :vk
         expect(controller.current_user).to eq user
       end
     end
