9af027143821acdaeac3d8439dc54b89cfb8d33a
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-09-14 14:55:16 +0300

    Fix deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+# config valid only for current version of Capistrano
+lock "3.9.0"
+
+set :application, "qna"
+set :repo_url, "https://github.com/Melchakovartem/thinknetica-project.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, "deployer"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+set :default_shell, '/bin/bash -l'
+
+
+namespace :deploy do
+
+  desc "Restart application"
+  task :resatrt do
+    on roles(:app), in: :sequence, wait: 5 do
+      #Your restart mechanism here
+      execute :touch, release_path.join("tmp/restart.txt")
+    end
+  end
+
+  after :publishing, :restart
+end
