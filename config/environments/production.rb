e45d12b3d1e2e0f1ab1db5d425e3247c6cd5705b
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-09-22 12:45:37 +0300

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   config.action_mailer.smtp_settings = {
    :address              => "smtp.mail.ru",
    :port                 => 587,
