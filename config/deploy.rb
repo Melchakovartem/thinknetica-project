5dc766386fdbd5ab90a34313238ed2ac16a8516f
Artem Melchakov <a.melchakov@boomstarter.ru>
2017-10-11 16:55:14 +0300

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.0"
+lock "3.9.1"
 
 set :application, "qna"
 set :repo_url, "https://github.com/Melchakovartem/thinknetica-project.git"
